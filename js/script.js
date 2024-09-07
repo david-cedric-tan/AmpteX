88e54d9f39a3ec5fc34756acae097f7d2f905a09
ANTiDOTE-21 <69837398+ANTiDOTE-21@users.noreply.github.com>
2024-09-08 04:42:15 +1000

    sample loading

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const video = document.getElementById('backgroundVideo');
+    if (video) {
+        video.playbackRate = 0.005; 
+    }
+});
+
+
+new Typed("#auto-type", {
+    strings: [" a Sustainable Future with Recycled Innovation."],
+    backSpeed: 65,
+    loop: true
+});
